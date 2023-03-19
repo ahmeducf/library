b342ffb8df0039ef94d139c6e018394185e8e456
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-03-20 01:21:25 +0200

    Create Event Listener stubs

--- src/main.js
+++ src/main.js
@@ -18,6 +18,11 @@ Library.prototype.addBook = (title, author, pages, isRead) => {
   }
 };
 
+// Functions
+function displayLibraryBooks() {}
+
+function updateLibrarySummary() {}
+
 // Library object and stub book objects
 const myLibrary = new Library();
 myLibrary.push(
@@ -27,3 +32,39 @@ myLibrary.push(
   true
 );
 myLibrary.push('The Prince', 'Niccolo Machiavelli', 250, false);
+
+// DOM
+const addNewBookButton = document.querySelector('form button[type="submit"]');
+const deleteAllBooksButton = document.querySelector('main .summary button');
+const removeBookButton = document.querySelectorAll(
+  'main .books-list tbody tr td:last-of-type i'
+);
+const readStatusButton = document.querySelectorAll(
+  'main .books-list tbody tr td:nth-child(4) i'
+);
+
+// Event Listeners
+window.addEventListener('load', () => {
+  displayLibraryBooks();
+  updateLibrarySummary();
+});
+
+addNewBookButton.addEventListener('click', (e) => {
+  console.dir(e);
+});
+
+deleteAllBooksButton.addEventListener('click', (e) => {
+  console.dir(e);
+});
+
+[...removeBookButton].forEach((book) => {
+  book.addEventListener('click', (e) => {
+    console.dir(e);
+  });
+});
+
+[...readStatusButton].forEach((button) => {
+  button.addEventListener('click', (e) => {
+    console.dir(e);
+  });
+});
