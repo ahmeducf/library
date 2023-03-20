6be42cbc7c6fd1fcdc5df88d0e2a818a2f041f49
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-03-20 20:26:04 +0200

    Display library content
    Implement `appendBookRowToBooksTable` and `displayLibraryBooks` functions

--- src/main.js
+++ src/main.js
@@ -11,12 +11,22 @@ function Book(title, author, pages, isRead) {
   this.isRead = isRead;
 }
 
-Library.prototype.addBook = (title, author, pages, isRead) => {
+Library.prototype.addBook = function addBook(title, author, pages, isRead) {
   this.libraryBooks.push(new Book(title, author, pages, isRead));
   if (isRead) {
     this.readBooksNumber += 1;
   }
 };
+
+// Library object and stub book objects
+const myLibrary = new Library();
+myLibrary.addBook(
+  'Computer Systems: A programmer perspective',
+  'Randal Bryant',
+  1500,
+  true
+);
+myLibrary.addBook('The Prince', 'Niccolo Machiavelli', 250, false);
 
 // DOM
 const addNewBookButton = document.querySelector('form button[type="submit"]');
@@ -29,7 +39,41 @@ const readStatusButton = document.querySelectorAll(
 );
 
 // Functions
-function displayLibraryBooks() {}
+function appendBookRowToBooksTable(title, author, pages, isRead) {
+  const tableBody = document.querySelector('main .books-list tbody');
+  const tr = document.createElement('tr');
+
+  const tdTitle = document.createElement('td');
+  const tdAuthor = document.createElement('td');
+  const tdPages = document.createElement('td');
+  const tdStatus = document.createElement('td');
+  const iStatusSymbol = document.createElement('i');
+  const tdRemove = document.createElement('td');
+  const iRemoveSymbol = document.createElement('i');
+
+  tdTitle.innerText = title;
+  tdAuthor.innerText = author;
+  tdPages.innerText = pages;
+
+  tdStatus.appendChild(iStatusSymbol);
+  if (isRead) {
+    iStatusSymbol.classList.add('fa-solid', 'fa-check', 'book-read');
+  } else {
+    iStatusSymbol.classList.add('fa-solid', 'fa-xmark', 'book-not-read');
+  }
+
+  tdRemove.appendChild(iRemoveSymbol);
+  iRemoveSymbol.classList.add('fa-solid', 'fa-trash-can');
+
+  tr.append(tdTitle, tdAuthor, tdPages, tdStatus, tdRemove);
+  tableBody.appendChild(tr);
+}
+
+function displayLibraryBooks() {
+  myLibrary.libraryBooks.forEach((book) => {
+    appendBookRowToBooksTable(book.title, book.author, book.pages, book.isRead);
+  });
+}
 
 function updateLibrarySummary() {}
 
@@ -58,13 +102,3 @@ deleteAllBooksButton.addEventListener('click', (e) => {
     console.dir(e);
   });
 });
-
-// Library object and stub book objects
-const myLibrary = new Library();
-myLibrary.addBook(
-  'Computer Systems: A programmer perspective',
-  'Randal Bryant',
-  1500,
-  true
-);
-myLibrary.addBook('The Prince', 'Niccolo Machiavelli', 250, false);
